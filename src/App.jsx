33a04420a3cad3a4930d6b0e97252be517d7436d
Kostiantyn <kog@yousee.dk>
2025-06-06 10:12:16 +0300

    add fonts Prettify  UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
 import Navigation from "./components/Navigation/Navigation";
 
-import HomePage from "./pages/HomePage/HomePage";
-import CampersPage from "./pages/CampersPage/CampersPage";
-import CamperDetailsPage from "./pages/CamperDetailsPage/CamperDetailsPage";
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const CampersPage = lazy(() => import("./pages/CampersPage/CampersPage"));
+const CamperDetailsPage = lazy(() =>
+  import("./pages/CamperDetailsPage/CamperDetailsPage")
+);
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CampersPage />} />
-        <Route path="/catalog/:id" element={<CamperDetailsPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CampersPage />} />
+          <Route path="/catalog/:id" element={<CamperDetailsPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,27 @@
-import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 import styles from "./Navigation.module.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navigation() {
+  const getNavLinkClass = ({ isActive }) => {
+    return clsx(styles.link, isActive && styles.active);
+  };
+
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>TravelTrucks</div>
-      <nav className={styles.nav}>
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) => (isActive ? styles.active : styles.link)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/catalog"
-          className={({ isActive }) => (isActive ? styles.active : styles.link)}
-        >
-          Catalog
-        </NavLink>
-      </nav>
+      <div className={clsx("container", styles.headerContainer)}>
+        <Link to="/">
+          <img alt="Site logo" src="/logo.png" />
+        </Link>
+        <nav className={styles.nav}>
+          <NavLink to="/" end className={getNavLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/catalog" end className={getNavLinkClass}>
+            Catalog
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
